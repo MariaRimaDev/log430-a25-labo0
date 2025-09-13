91c9b39e2b12ca5dce96464ae71697313fbb656a
MariaRimaDev <mariarima7@gmail.com>
2025-09-13 14:45:06 -0400

    Activite 2

--- src/tests/test_calculator.py
+++ src/tests/test_calculator.py
@@ -6,8 +6,23 @@ Auteurs : Gabriel C. Ullmann, Fabio Petrillo, 2025
 
 from calculator import Calculator
 
-def test_app():
-    my_calculator = Calculator()
-    assert my_calculator.get_hello_message() == "Calculatrice"
-
 # TODO: ajoutez les tests
+
+"=========================== ACTIVITÉ 1 ==========================="
+
+my_calculator = Calculator()
+
+def test_addition():
+    assert my_calculator.addition(2, 3) == 5
+
+def test_subtraction():
+    assert my_calculator.subtraction(5, 3) == 2
+
+def test_multiplication():
+    assert my_calculator.multiplication(4, 3) == 12
+
+def test_division():
+    assert my_calculator.division(10, 2) == 5
+
+def test_addition_erreur():
+    assert my_calculator.addition(2, 2) == 5
